c852c9dd61f8dcf4543e53be5f6d9fad17918ba3
fengtingzhang <fengtingzhang@gmail.com>
2015-06-14 19:54:22 -0400

    Modified styles, adding font-awesome icons

--- public/javascripts/src/Domain.jsx
+++ public/javascripts/src/Domain.jsx
@@ -4,22 +4,16 @@ module.exports = React.createClass({
     render: function() {
         return (
             <a href="#" className="list-group-item">
-                <h4 class="list-group-item-heading listing-prod">
-                    <span className="label label-default">{ this.props.category }</span>
+                <h4 className="list-group-item-heading listing-prod">
+                    <i className="fa fa-cube"></i><span className="listing-realm">{ this.props.name }</span>
+                    <span className="listing-category label label-default">{ this.props.category }</span>                    
                 </h4>
-                <p className="list-group-item-text">
-                    <span className="listing-realm">PROD: { this.props.prod }</span>
-                    <small className="listing-category">Category: { this.props.name }</small>
-                </p>
-                <p className="list-group-item-text">
-                    <span className="listing-realm">QA: { this.props.qa }</span>
-                </p>
-                <p className="list-group-item-text">
-                    <span className="listing-realm">INT: { this.props.int }</span>
-                </p>
-                <p className="list-group-item-text">
-                    <span className="listing-realm">DEV: { this.props.dev }</span>
-                </p>
+                <div class="btn-group" role="group" aria-label="...">
+                    <button type="button" class="btn btn-default">PROD: { this.props.prod }</button>
+                    <button type="button" class="btn btn-default">QA: { this.props.qa }</button>
+                    <button type="button" class="btn btn-default">INT: { this.props.int }</button>
+                    <button type="button" class="btn btn-default">DEV: { this.props.dev }</button>                    
+                </div>
             </a>
         );
     }
